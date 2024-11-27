ed6d1fb1e61432835618e1b400d6173a04a8a51a
kyogo.930.red <kyogo.930.red@gmail.com>
2024-11-27 05:17:30 +0000

    市区町村のサンプルデータinsert

--- lib/city_list_page.dart
+++ lib/city_list_page.dart
@@ -5,12 +5,34 @@ class CityListPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    const cities = [
+      '札幌市',
+      '仙台市',
+      'さいたま市',
+      '千葉市',
+      '横浜市',
+      '川崎市',
+      '名古屋市',
+      '京都市',
+      '大阪市',
+      '福岡市',
+    ];
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('市区町村一覧'),
       ),
-      body: const Center(
-        child: Text('市区町村の一覧画面です'),
+      body: ListView(
+        children: [
+          for (final city in cities)
+            ListTile(
+                title: Text(city),
+                subtitle: const Text('主要都市'), // 政令指定都市や中核市など
+                trailing: const Icon(Icons.navigate_next),
+                onTap: () {
+                  // TODO 詳細画面遷移
+                }),
+        ],
       ),
     );
   }
