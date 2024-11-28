f249445bdd19c3ee951f4a3ab8197558dc771423
kyogo.930.red <kyogo.930.red@gmail.com>
2024-11-28 04:54:09 +0000

    futureメソッド

--- lib/city_list_page.dart
+++ lib/city_list_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:myapp/city_detail_page.dart';
 
 class CityListPage extends StatelessWidget {
   const CityListPage({super.key});
@@ -22,17 +23,32 @@ class CityListPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('市区町村一覧'),
       ),
-      body: ListView(
-        children: [
-          for (final city in cities)
-            ListTile(
-                title: Text(city),
-                subtitle: const Text('主要都市'), // 政令指定都市や中核市など
-                trailing: const Icon(Icons.navigate_next),
-                onTap: () {
-                  // TODO 詳細画面遷移
-                }),
-        ],
+      body: FutureBuilder<void>(
+        future: Future.delayed(const Duration(seconds: 3)),
+        builder: (context, snapshot) {
+          if(snapshot.connectionState == ConnectionState.waiting){
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return ListView(
+            children: [
+              for (final city in cities)
+                ListTile(
+                    title: Text(city),
+                    subtitle: const Text('政令指定都市'),
+                    trailing: const Icon(Icons.navigate_next),
+                    onTap: () {
+                      // TODO 詳細画面遷移
+                      Navigator.of(context).push(
+                        MaterialPageRoute(
+                          builder: (context) => CityDetailPage(city: city),
+                        ),
+                      );
+                    }),
+            ],
+          );
+        }
       ),
     );
   }
